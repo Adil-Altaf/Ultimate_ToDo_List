c3d1171010104786d0d11316b2ed52d8e7c9de63
Hamzakhann <hamzakhann66@gmail.com>
2018-09-14 01:32:03 +0500

    added database with server and push some data using postman

--- dev/app.ts
+++ dev/app.ts
@@ -1,16 +1,19 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
 import { Routes } from "./routes/routes";   
 
 class App {
 
     public app: express.Application;
     public route: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://todo:alpha1411@ds155862.mlab.com:55862/ultimate_todo_app'
 
     constructor() {
         this.app = express();
         this.config();
         this.route.routes(this.app);
+        this.mongoSetup();
     }
 
     private config(): void{
@@ -18,6 +21,11 @@ class App {
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private mongoSetup(): void{
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl , {useNewUrlParser : true});    
     }
 
 }
