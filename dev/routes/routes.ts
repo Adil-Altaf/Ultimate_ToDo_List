d07802f1cfa5eb8aeffafe99ecbf224a65e6d5f7
Hamzakhann <hamzakhann66@gmail.com>
2018-09-13 21:12:21 +0500

    initilize crud routes for task

--- dev/routes/routes.ts
+++ dev/routes/routes.ts
@@ -1,12 +1,40 @@
 import {Request, Response} from "express";
 
 export class Routes {      
-    public routes(app): void {          
+    public routes(app): void {     
+
         app.route('/todo/api/v1.0/tasks')
         .get((req: Request, res: Response) => {            
             res.status(200).send({
                 message: 'GET request successfulll!!!!'
             })
-        })               
+        })
+        
+        app.route('/todo/api/v1.0/tasks') 
+        .post((req: Request, res: Response) => {           
+            res.status(200).send({
+                message: 'GET request successfulll!!!!'
+            })
+        }) 
+        
+        app.route('/todo/api/v1.0/tasks/:task_id')
+        .get((req: Request, res: Response) => {
+            res.status(200).send({
+                message: 'GET request successfulll!!!!'
+            })
+        })
+        
+        .put((req: Request, res: Response) => {          
+                res.status(200).send({
+                    message: 'PUT request successfulll!!!!'
+                })
+         })
+
+         .delete((req: Request, res: Response) => {
+             
+                res.status(200).send({
+                    message: 'DELETE request successfulll!!!!'
+                })
+        })
     }
 }
