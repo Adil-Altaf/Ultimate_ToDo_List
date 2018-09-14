75b3488f9162f200cd813afbeea5db0b9fa3b9bf
NajamShehzad <najambutt195@gmail.com>
2018-09-14 21:57:23 +0500

    Render Todo List

--- ultimate_todo_app_client/src/screens/TodoScreen/Todo.js
+++ ultimate_todo_app_client/src/screens/TodoScreen/Todo.js
@@ -6,23 +6,49 @@ class Todo extends Component {
         super();
         this.state = {
             title: '',
-            description: ''
+            description: '',
+            todos: []
         }
+        this.submitForm = this.submitForm.bind(this);
     }
 
-    updateTitle(title){
-        console.log(title);
-        this.setState({title:title}) 
+    updateTitle(title) {
+        this.setState({ title: title })
+    }
+
+    updateDescription(description) {
+        this.setState({ description: description });
+    }
+
+    submitForm(e) {
+        e.preventDefault();
+        const { title, description, todos } = this.state;
+        todos.push({ title, description });
+        this.setState({ todos, title: '', description: '' });
+    }
+
+    deleteList(index) {
+        const { todos } = this.state;
+        let updated = todos.filter((item, index1) => {
+            if (index1 !== index) {
+                return true
+            }
+
+        });
+        this.setState({todos:updated})
+        console.log(updated);
+
+
     }
 
     inputField() {
         const { title, description } = this.state;
         return (
-            <form className="form" >
+            <form className="form" onSubmit={this.submitForm} >
                 <div>
                     <label style={{ marginRight: 2 }}>
                         Title
-                    <input
+                        <input
                             value={title}
                             onChange={(e) => this.updateTitle(e.target.value)}
                             type="text"
@@ -31,7 +57,9 @@ class Todo extends Component {
                     </label>
                     <label style={{ marginLeft: 2 }} >
                         Description
-                    <input
+                        <input
+                            value={description}
+                            onChange={(e) => this.updateDescription(e.target.value)}
                             type="text"
                             className="form-control"
                         />
@@ -41,6 +69,22 @@ class Todo extends Component {
                     Add Todo
                 </button>
             </form>
+        )
+    }
+    unorderList() {
+        const { todos } = this.state;
+        return (
+            <ul>
+                {todos.map((list, index) => {
+                    return (
+                        <li>
+                            {list.title}-{list.description}
+                            <button>Edit</button>
+                            <button onClick={() => this.deleteList(index)} >Delete</button>
+                        </li>
+                    )
+                })}
+            </ul>
         )
     }
 
@@ -53,6 +97,7 @@ class Todo extends Component {
                     TodoApp
                 </h1>
                 {this.inputField()}
+                {this.unorderList()}
             </div>
         )
     }
