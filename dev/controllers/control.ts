b4096b669109286c7af17fd92e970ca341537ad9
NajamShehzad <najambutt195@gmail.com>
2018-09-14 14:14:24 +0500

    Fix Some bugs

--- dev/controllers/control.ts
+++ dev/controllers/control.ts
@@ -8,57 +8,57 @@ import { Request, Response } from 'express';
 const Task = mongoose.model('Task', TaskSchema);
 
 //we use all busineess logic in this class this is reponsible for all things
-export class TaskController{
+export class TaskController {
 
     //This logic is for add new task in database
-    public addNewTask (req: Request, res: Response) {                
+    public addNewTask(req: Request, res: Response) {
         let newTask = new Task(req.body);
         newTask.save((err, task) => {
-            if(err){
-                res.send(err);
-            }    
-            res.json(task);
+            if (err) {
+                return res.send(err);
+            }
+            return res.json(task);
         });
     }
 
     //This logic is for get all the task from the database
-    public getAllTask (req: Request, res: Response) {           
+    public getAllTask(req: Request, res: Response) {
         Task.find({}, (err, tasks) => {
-            if(err){
-                res.send(err);
+            if (err) {
+                return res.send(err);
             }
-                res.json(tasks);
+            return res.json(tasks);
         });
-    
+
     }
 
     //This logic is for get an individual task by its id
-    public getTask (req: Request, res: Response) {           
+    public getTask(req: Request, res: Response) {
         Task.findById(req.params.task_id, (err, task) => {
-            if(err){
-                res.send(err);
+            if (err) {
+                return res.send(err);
             }
-            res.json(task);
+            return res.json(task);
         });
     }
 
     //This logic is for update task by finding task with its id and then update with new data provided
-    public updateTask (req: Request, res: Response) {           
-        Task.findOneAndUpdate({ _id: req.params.task_id }, 
-            req.body , { new: true }, (err, task) => {
-            if(err){
-                res.send(err);
-            }
-            res.json(task);
-        });
+    public updateTask(req: Request, res: Response) {
+        Task.findOneAndUpdate({ _id: req.params.task_id },
+            req.body, { new: true }, (err, task) => {
+                if (err) {
+                    return res.send(err);
+                }
+                return res.json(task);
+            });
     }
     //This logic is for delete a task by its id
-    public deleteTask (req: Request, res: Response) {           
+    public deleteTask(req: Request, res: Response) {
         Task.remove({ _id: req.params.task_id }, (err, task) => {
-            if(err){
-                res.send(err);
+            if (err) {
+                return res.send(err);
             }
-            res.json({ message: 'Successfully deleted Task!'});
+            return res.json({ message: 'Successfully deleted Task!' });
         });
     }
 
