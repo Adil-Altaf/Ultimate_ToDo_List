195d94c1861677f6244c88d73764a454deea59c7
NajamShehzad <najambutt195@gmail.com>
2018-09-15 14:35:34 +0500

    Test Completed

--- ultimate_todo_app_client/src/App.test.js
+++ ultimate_todo_app_client/src/App.test.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
 import App from './App';
 import Todo from './screens/TodoScreen/Todo';
 import NavBar from './component/NavBar/NavBar';
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('NavBar', () => {
+    it('NavBar renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<NavBar />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+    test('also has a valid snapshot', () => {
+        const component = renderer.create(
+            <NavBar />
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
 
-it('renders Todo List  without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Todo />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+    test('has a valid snapshot', () => {
+        const component = renderer.create(
+            <App />
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
 
-it('Header Should Work Fine', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NavBar>Some Text</NavBar>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Todos', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Todo />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+    test('has a valid snapshot', () => {
+        const component = renderer.create(
+            <Todo />
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
+
