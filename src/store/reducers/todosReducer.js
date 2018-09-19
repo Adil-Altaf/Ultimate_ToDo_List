ccecd28159e1147b53fbe25981837baa36c2ef67
muhammadali448 <muhammadali19960@outlook.com>
2018-09-19 13:26:01 +0500

    implemented fetch reducer

--- src/store/reducers/todosReducer.js
+++ src/store/reducers/todosReducer.js
@@ -1,18 +1,18 @@
-
-import { FETCH_TODOS, FETCH_ERROR } from '../actions/types'
+import { FETCH_TODOS, FETCH_ERROR } from "../actions/types";
 const INITIAL_STATE = {
-    todos: null
-}
+  todos: null
+};
 
 const TodosReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case FETCH_TODOS:
-            return {
-                ...state, todos: action.payload
-            }    
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case FETCH_TODOS:
+      return {
+        ...state,
+        todos: action.payload
+      };
+    default:
+      return state;
+  }
+};
 
 export default TodosReducer;
