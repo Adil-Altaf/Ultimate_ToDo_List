6aff7419032a9c7006af5c3a944fcfd7a4a515b6
muhammadali448 <muhammadali19960@outlook.com>
2018-09-20 09:52:13 +0500

    delete reducer fixed

--- src/store/reducers/todosReducer.js
+++ src/store/reducers/todosReducer.js
@@ -1,18 +1,22 @@
-import { FETCH_TODOS, FETCH_ERROR } from "../actions/types";
+
+import { FETCH_TODOS, FETCH_ERROR, DELETE_TODO } from '../actions/types'
 const INITIAL_STATE = {
-  todos: null
-};
+    todos: null
+}
 
 const TodosReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case FETCH_TODOS:
-      return {
-        ...state,
-        todos: action.payload
-      };
-    default:
-      return state;
-  }
-};
+    switch (action.type) {
+        case FETCH_TODOS:
+            return {
+                ...state, todos: action.payload
+            }    
+        case DELETE_TODO:
+            return {
+                 ...state, todos: state.todos.filter(todo => todo.id !== action.payload)
+            }    
+        default:
+            return state;
+    }
+}
 
 export default TodosReducer;
