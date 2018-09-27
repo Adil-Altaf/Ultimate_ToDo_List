7bed1b06d6314e9b29505f253e13c10705343133
saqibarfeen <saqib.arfeen@cloud9net.com>
2018-09-27 07:10:21 +0500

    mongodb Schema definition

---
+++ Nosql/model/todo.js
@@ -0,0 +1,22 @@
+const Mongoose = require("mongoose");
+
+const todo = new Mongoose.Schema({
+  todo_id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  done: {
+    type: Boolean,
+    required: true
+  }
+});
+module.exports = Mongoose.model("todo", todo);
